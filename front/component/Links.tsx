4590364c5eb104c873984a593174efb92dca1726
im-neko <yuki@ideta.net>
2019-07-04 14:22:21 +0900

    update

--- front/component/Links.tsx
+++ front/component/Links.tsx
@@ -6,10 +6,18 @@ export default class Accounts extends Component<IProps> {
     return (
     <div>
       <div className="siimple-alert siimple-alert--green">
+        [Portfolio的なやつ]
+      </div>
+      <a href="https://www.resume.id/">resume</a>
+      <br />
+      <a href="https://portfolio.forkwell.com/">forkwell</a> 
+
+      <div className="siimple-alert siimple-alert--green">
         [相互リンク様]
       </div>
       <a href="https://rhenium.dev">れにうむのポートフォリオ</a>
       <br/>
+
     </div>
     )
   }
