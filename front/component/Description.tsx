a3c0811eaf06f992ffa497e5df12375859e13919
im-neko <yuki@ideta.net>
2019-09-29 21:41:08 +0900

    update

--- front/component/Description.tsx
+++ front/component/Description.tsx
@@ -1,16 +1,15 @@
 import React, { Component } from "react";
 
 
-export default class Description extends Component<IProps> {
+export default class Description extends Component{
   render () {
     return (
       <div>
         <div className="siimple-alert siimple-alert--purple">
           [Description]<br/>
         </div>
-        Webアプリとかサービスの受託開発とかやってます。<br/>
-        Node,pyhton3で色々作ります<br/>
-        Webのお仕事受け付けてますますます<br/>
+        Web関係全般の受託開発やってます。<br/>
+        Node/Typescript,pyhton3,Golangで色々作ります<br/>
         SuzuriでHTTPステータスグッズ売ってます<br/>
         <a href="https://suzuri.jp/Im_nuko">https://suzuri.jp/Im_nuko</a><br/>
       </div>
