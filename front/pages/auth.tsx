7c9ce61536f1b808064519eb0069d2d5e8b7df82
im-neko <yuki@ideta.net>
2019-10-07 15:46:32 +0900

    add auth page

---
+++ front/pages/auth.tsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import AdSense from 'react-adsense';
+
+import Header from '../component/Header';
+import Accounts from '../component/Accounts';
+import Description from '../component/Description';
+import Links from '../component/Links';
+
+
+export default class Auth extends Component<IProps> {   
+  render() {
+    return (
+      <div style={style}>
+        <link href="/static/css/siimple.min.css" rel="stylesheet" />
+        <center>
+          <Header />
+          <Accounts />
+          <Description />
+          <Links />
+          <AdSense.Google
+            client='ca-pub-3933354234319712'
+            slot='5700255991'
+            style={{ display: 'block' }}
+            format='auto'
+            responsive='true'
+            layoutKey='-gw-1+2a-9x+5c'
+          />
+        </center>
+      </div>
+    );
+  }
+}
+
+const style = {
+  margin: 0,
+  padding: 0,
+  fontFamily: "sans-serif",
+}
